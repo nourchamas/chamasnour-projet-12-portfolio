7adf7b98bb8da2bbdd2484f5b7f7a85dc8836c74
nourchamas <153526452+nourchamas@users.noreply.github.com>
2024-06-11 15:21:30 +0000

    Initial commit

---
+++ src/data/info.ts
@@ -0,0 +1,65 @@
+export const info = {
+  name: "Nour chamas ",
+  brief_description:
+    "une développeuse front-end spécialisée en HTML, CSS, JavaScript, React et Redux. ",
+  role: "developpeuse front-end",
+  picture: "/",
+  picture_alt: "Nour chamas",
+  location: "france",
+
+  about: {
+    description: `Je suis une développeuse front-end spécialisée en HTML, CSS, JavaScript, React et Redux. Passionnée par la création d'interfaces utilisateur intuitives et réactives, 
+    je transforme des concepts créatifs en expériences web interactives et performantes. Mon objectif est de concevoir des sites et applications web esthétiques et fonctionnels, garantissant une expérience utilisateur optimale.
+   .`,
+    education: [
+      {
+        title: "formation integrateur web front-end ",
+        date: "2023-2024",
+        location: "openclassroom a distance",
+      },
+    ],
+    experience: [{}, {}, {}],
+
+    skills: ["Html", "Css", "JavaScript", "React", "Node.js"], // not used yet
+  },
+
+  projects: [
+    {
+      title: "Booki",
+      date: "2023",
+      description:
+        "J'ai créé la page d'accueil pour une agence de voyage, mettant en avant les destinations populaires, les offres spéciales et les services offerts par l'agence",
+      link: "https://github.com/nourchamas/Booki.git",
+      tech: ["Html", "css"],
+      img_alt: "booki",
+      img_path: "/public/booki projet.png ",
+    },
+    {
+      title: "Kasa",
+      date: "2024",
+      description:
+        "Créez une application web de location immobilière avec React",
+      link: "https://github.com/nourchamas/kasa.git",
+      tech: ["Html", "Css", "React", "Node.js", "Javascript"],
+      img_alt: "kasa photo ",
+      img_path: "/public/kasa photo.png",
+    },
+    {
+      title: "Debuggez le site ",
+      date: "2024",
+      description:
+        "Dans ce projet, j'ai pris en charge la tâche de déboguer et d'améliorer les performances du site web d'une agence d'événementiel. L'objectif était de corriger les erreurs existantes, d'optimiser le code et de garantir une expérience utilisateur fluide et sans faille.",
+      link: "https://github.com/nourchamas/Debuggez-le-site_projet10.git",
+      tech: ["Html", "Css", "Scss", "React", "Node.js"],
+      img_alt: "724 events",
+      img_path: "/public/724 events.png",
+    },
+  ],
+
+  contact: {
+    email: "nour.chamas@outlook.com",
+    linkedin: "https://www.linkedin.com/in/bartsimpsonfake",
+    github: "https://github.com/nourchamas",
+    twitter: "https://www.twitter.com/bartsimpsonfake",
+  },
+};
